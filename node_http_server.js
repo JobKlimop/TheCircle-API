a15ccd33f45fa633426bc2fc9c753c67a7b9b032
QS <qsluong1@gmail.com>
2018-06-19 13:42:43 +0200

    Added 'authorization' in headers

--- node_http_server.js
+++ node_http_server.js
@@ -38,7 +38,7 @@ class NodeHttpServer {
 
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", '*' );
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
 
